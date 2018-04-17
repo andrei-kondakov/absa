86c97e0c8b369708cae6af61b266bcf9d1379875
Andrei Kondakov <and.kondakov94@gmail.com>
2018-04-18 01:02:39 +0300

    Keras_ml // Add keras sessions view

--- absa/keras_ml/urls.py
+++ absa/keras_ml/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 import keras_ml.views
 
 urlpatterns = [
+    path('sessions', keras_ml.views.sessions, name='sessions'),
     path('session/<int:session_id>', keras_ml.views.session_detail, name='session_detail')
 ]
--- absa/keras_ml/views.py
+++ absa/keras_ml/views.py
@@ -1,6 +1,30 @@
+from collections import defaultdict
+
 from django.shortcuts import get_object_or_404, render
 
+from data.models import Task
 from keras_ml.models import TrainSession
+
+
+def sessions(request):
+    data = {
+        'tasks': []
+    }
+    tasks = Task.objects.all()
+
+    for task in tasks:
+        train_sessions = defaultdict(list)
+        for session in TrainSession.objects.filter(batch__task_id=task.id):
+            train_sessions[session.model_id].append(session)
+
+        data['tasks'].append(
+            {
+                'task': task,
+                'sessions': dict(train_sessions)
+            }
+        )
+
+    return render(request, 'keras_ml/sessions.html', data)
 
 
 def session_detail(request, session_id):
